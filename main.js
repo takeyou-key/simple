b6ea6eee873e800b0499a40ad51da13f1ca839b5
takeyou-key <yuk.takizawa@gmail.com>
2024-10-09 15:18:03 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+$(function () {
+    $('.toggle-btn').click(function () {
+        $('#header').toggleClass('open')
+    });
+    $('#nav').click(function () {
+        $('#header').removeClass('open')
+    });
+    $('#nav a').click(function () {
+        $('#header').removeClass('open')
+    });
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1000) { // 100pxスクロールしていたら上に戻るボタンを表示
+        $(".to-top").fadeIn();
+    } else {
+        $(".to-top").fadeOut();
+    }
+});
+$(".to-top").click(function () {
+    $("body,html").animate({ scrollTop: 0 }, 800); // 800msかけて上に戻る
+});
